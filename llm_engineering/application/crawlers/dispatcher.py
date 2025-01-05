91390b34d3fc3b0db475095fcd06b4038589239f
Leo Chen <leoikaichen@gmail.com>
2025-01-05 12:55:53 -0800

    finished most of data pipeline ch 3

--- llm_engineering/application/crawlers/dispatcher.py
+++ llm_engineering/application/crawlers/dispatcher.py
@@ -9,7 +9,8 @@ from .github import GithubCrawler
 from .linkedin import LinkedInCrawler
 from .medium import MediumCrawler
 
-class CrawlerDispatcher():
+
+class CrawlerDispatcher:
     def __init__(self) -> None:
         self._crawlers = {}
 
@@ -18,35 +19,33 @@ class CrawlerDispatcher():
         dispatcher = cls()
 
         return dispatcher
-    
+
     def register_medium(self) -> "CrawlerDispatcher":
-        self.register("https://www.medium.com", MediumCrawler)
+        self.register("https://medium.com", MediumCrawler)
 
         return self
-    
+
     def register_linkedin(self) -> "CrawlerDispatcher":
-        self.register("https://www.linkedin.com", LinkedInCrawler)
+        self.register("https://linkedin.com", LinkedInCrawler)
 
         return self
 
-    def register_gihub(self) -> "CrawlerDispatcher":
-        self.register("https://www.github.com", GithubCrawler)
+    def register_github(self) -> "CrawlerDispatcher":
+        self.register("https://github.com", GithubCrawler)
 
         return self
-    
-    def register(self, domain: str, crawler = type[BaseCrawler]):
+
+    def register(self, domain: str, crawler: type[BaseCrawler]) -> None:
         parsed_domain = urlparse(domain)
         domain = parsed_domain.netloc
 
         self._crawlers[r"https://(www\.)?{}/*".format(re.escape(domain))] = crawler
 
-
-        
     def get_crawler(self, url: str) -> BaseCrawler:
         for pattern, crawler in self._crawlers.items():
             if re.match(pattern, url):
                 return crawler()
         else:
             logger.warning(f"No crawler found for {url}. Defaulting to CustomArticleCrawler.")
-        
-        return CustomArticleCrawler()
+
+            return CustomArticleCrawler()
